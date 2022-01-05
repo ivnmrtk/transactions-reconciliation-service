ec8863e920483076fe49eee6fbc2750dbd74cd99
ivnmrtk <ivnmrtk@gmail.com>
2022-01-05 22:00:15 +0300

    initial commit

---
+++ src/main/java/com/github/ivnmrtk/transactionsreconciliationservice/TransactionsReconciliationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.ivnmrtk.transactionsreconciliationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsReconciliationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsReconciliationServiceApplication.class, args);
+    }
+
+}
